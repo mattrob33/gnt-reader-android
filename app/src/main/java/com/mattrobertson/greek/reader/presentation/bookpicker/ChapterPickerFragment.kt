03d829c6933ee5268efd9c5b6e0ea8fdfdc2b587
Matt Robertson <mattrobertson333@gmail.com>
2020-06-23 07:33:36 -0500

    1. Removed Settings from bottom nav. 2. Some fixes for day/night colors.

--- app/src/main/java/com/mattrobertson/greek/reader/presentation/bookpicker/ChapterPickerFragment.kt
+++ app/src/main/java/com/mattrobertson/greek/reader/presentation/bookpicker/ChapterPickerFragment.kt
@@ -5,9 +5,13 @@ import android.content.Intent
 import android.graphics.Color
 import android.graphics.Point
 import android.os.Bundle
-import android.view.*
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.LinearLayout
 import androidx.appcompat.widget.AppCompatButton
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.mattrobertson.greek.reader.R
@@ -20,14 +24,6 @@ class ChapterPickerFragment : Fragment() {
 
     private val args: ChapterPickerFragmentArgs by navArgs()
 
-    var book = 0
-    var numChapters = 1
-
-
-    companion object {
-        fun newInstance() = ChapterPickerFragment()
-    }
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.chapter_picker, container, false)
     }
@@ -35,43 +31,27 @@ class ChapterPickerFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        book = args.book
+        val numChapters = numChaptersInBook(args.book)
 
-        numChapters = numChaptersInBook(book)
-
-        val display: Display = requireActivity().windowManager.defaultDisplay
-
+        val display = requireActivity().windowManager.defaultDisplay
         val size = Point()
         display.getSize(size)
-        val wScreen = size.x - 40
 
+        val wScreen = size.x - 40
         var wTarget = dpToPx(requireContext(), 60)
         var numSq = wScreen / wTarget // # of squares per row
 
-
-        // *** Attempted hack to fix display issues *** \\
         if (numSq < 4) {
             numSq = 4
             wTarget = wScreen / numSq
-            /*
-			* Note: Ideally numSq is calculated on screen size (allows for
-			* devices of phones/tablets in portrait/landscape. But if a
-			* is so small that it has only 1-3 per row with the preferred
-			* button width, force it to have 4 per row and recalculate the
-			* preferred button width based on this number.
-			*/
         }
 
         val rem = wScreen % wTarget // total remaining space on each row
-
         val remPer = rem / numSq // remaining pixels per square, per row
-
         val wActual = wTarget + remPer
         val numRows = (numChapters + numSq - 1) / numSq // # of rows needed
 
-
         var curRow: ButtonRow
-
         var btn: ChapterButton
 
         var ch = 1
@@ -82,15 +62,15 @@ class ChapterPickerFragment : Fragment() {
             for (j in 0 until numSq) {
                 val chap = ch
                 btn = ChapterButton(requireContext(), wActual)
-                btn.text = "" + ch
+                btn.text = "$ch"
                 curRow.addView(btn)
-                btn.setOnClickListener(View.OnClickListener {
+                btn.setOnClickListener {
                     Intent(requireContext(), ReaderActivity::class.java).apply {
-                        putExtra("book", book)
+                        putExtra("book", args.book)
                         putExtra("chapter", chap)
                         startActivity(this)
                     }
-                })
+                }
                 ch++
                 if (ch > numChapters) break
             }
@@ -101,7 +81,7 @@ class ChapterPickerFragment : Fragment() {
         init {
             layoutParams = LinearLayout.LayoutParams(sidePx, sidePx)
             gravity = Gravity.CENTER
-            background = resources.getDrawable(R.drawable.chapter_button)
+            background = ResourcesCompat.getDrawable(resources, R.drawable.chapter_button, null)
             setTextColor(Color.WHITE)
         }
     }
