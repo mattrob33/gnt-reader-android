8f0b361078d3c4a3dbcf6615b996fd829d9bd0cf
Matt Robertson <mattrobertson333@gmail.com>
2020-06-23 12:18:16 -0500

    Added menu to home toolbar

--- app/src/main/java/com/mattrobertson/greek/reader/presentation/HomeFragment.kt
+++ app/src/main/java/com/mattrobertson/greek/reader/presentation/HomeFragment.kt
@@ -1,23 +1,45 @@
 package com.mattrobertson.greek.reader.presentation
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.mattrobertson.greek.reader.R
 import kotlinx.android.synthetic.main.home_fragment.*
 
+
 class HomeFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.home_fragment, container, false)
+        val view = inflater.inflate(R.layout.home_fragment, container, false)
+        setHasOptionsMenu(true)
+        return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.home_toolbar_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_item_settings -> {
+                requireActivity().findNavController(R.id.core_nav_host_fragment).navigate(
+                        HomeFragmentDirections.homeToSettings()
+                )
+            }
+        }
+        return true
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        (activity as AppCompatActivity).setSupportActionBar(home_toolbar)
+        (activity as AppCompatActivity).supportActionBar?.setDisplayShowTitleEnabled(false)
 
         val navController = findNavController(requireActivity(), R.id.nav_host_fragment)
         bottom_nav_view.setupWithNavController(navController)
