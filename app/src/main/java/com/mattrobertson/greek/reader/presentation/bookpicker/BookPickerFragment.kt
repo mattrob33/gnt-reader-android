9f26f59285ac60802bf86e7a4be0bc2ff0023146
Matt Robertson <mattrobertson333@gmail.com>
2020-06-30 14:28:27 -0500

    Added UI view for recents

--- app/src/main/java/com/mattrobertson/greek/reader/presentation/bookpicker/BookPickerFragment.kt
+++ app/src/main/java/com/mattrobertson/greek/reader/presentation/bookpicker/BookPickerFragment.kt
@@ -10,16 +10,36 @@ import android.view.ViewGroup
 import android.widget.AbsListView
 import android.widget.AdapterView.OnItemClickListener
 import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.mattrobertson.greek.reader.CoreNavigationDirections
 import com.mattrobertson.greek.reader.R
 import com.mattrobertson.greek.reader.ReaderActivity
+import com.mattrobertson.greek.reader.data.Settings
+import com.mattrobertson.greek.reader.model.GntVerseRef
 import com.mattrobertson.greek.reader.presentation.HomeFragmentDirections
+import com.mattrobertson.greek.reader.util.getBookTitle
 import com.mattrobertson.greek.reader.util.isSingleChapterBook
 import kotlinx.android.synthetic.main.book_picker_fragment.*
-import kotlinx.android.synthetic.main.home_fragment.*
 
 class BookPickerFragment : Fragment() {
+
+    private val recentsAdapter: RecentsAdapter by lazy {
+
+        Settings.getInstance(requireContext()).loadRecents()
+
+        RecentsAdapter(object: RecentsAdapter.OnRecentSelectedListener {
+            override fun onRecentItemSelected(ref: GntVerseRef) {
+                requireActivity().findNavController(R.id.core_nav_host_fragment).navigate(
+                        CoreNavigationDirections.toReader(ref.book, ref.chapter)
+                )
+            }
+        })
+
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.book_picker_fragment, container, false)
@@ -27,6 +47,9 @@ class BookPickerFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        recents_list.adapter = recentsAdapter
+        recents_list.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
 
         val arrBooks = resources.getStringArray(R.array.books)
         val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, arrBooks)
@@ -48,18 +71,23 @@ class BookPickerFragment : Fragment() {
         }
 
         lvBooks.setOnScrollListener(object: AbsListView.OnScrollListener {
-            override fun onScroll(view: AbsListView?, firstVisibleItem: Int, visibleItemCount: Int, totalItemCount: Int) {
-                view?.let {
-                    val isTop = it.scrollY == 0
-                    notifyScrollChange(isTop)
-                }
+            override fun onScroll(view: AbsListView, firstVisibleItem: Int, visibleItemCount: Int, totalItemCount: Int) {
+                val isTop = lvBooks.firstVisiblePosition == 0
+                notifyScrollChange(isTop)
             }
 
             override fun onScrollStateChanged(view: AbsListView?, scrollState: Int) {}
         })
+
+
     }
 
     private fun notifyScrollChange(isTop: Boolean) {
-        home_toolbar?.background = ColorDrawable(Color.LTGRAY)
+//        (activity as AppCompatActivity).supportActionBar?.setBackgroundDrawable(
+//                if (isTop)
+//                    ColorDrawable(Color.DKGRAY)
+//                else
+//                    ColorDrawable(Color.LTGRAY)
+//        )
     }
 }
