659429d9ebe331be39389cacb0cf1cb4bff92483
Matt Robertson <mattrobertson333@gmail.com>
2020-06-30 10:19:53 -0500

    Added Recents manager and tests for it

--- app/src/main/java/com/mattrobertson/greek/reader/model/GntVerseRef.kt
+++ app/src/main/java/com/mattrobertson/greek/reader/model/GntVerseRef.kt
@@ -1,7 +1,9 @@
 package com.mattrobertson.greek.reader.model
 
+import com.mattrobertson.greek.reader.util.AppConstants.NO_VERSE
+
 data class GntVerseRef (
         val book: Int,
         val chapter: Int,
-        val verse: Int
+        val verse: Int = NO_VERSE
 )
