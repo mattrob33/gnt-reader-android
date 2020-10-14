d7e46940ede4d8b935610f07780dc43ec0853007
Matt Robertson <mattrobertson333@gmail.com>
2020-10-14 14:06:13 -0500

    Added compareTo and equals implementations for Book and VerseRef

--- app/src/main/java/com/mattrobertson/greek/reader/model/VerseRef.kt
+++ app/src/main/java/com/mattrobertson/greek/reader/model/VerseRef.kt
@@ -7,7 +7,7 @@ data class VerseRef (
 	val book: Book,
 	val chapter: Int,
 	val verse: Int
-) {
+) : Comparable<VerseRef> {
 	/**
 	 * Validates inputs. Note that Book is self-validating and is thus not validated here.
  	 */
@@ -26,6 +26,33 @@ data class VerseRef (
 
 		require(verse <= numVersesInChapter(book, chapter)) {
 			"${book.title} $chapter does not have $verse verses"
+		}
+	}
+
+	override fun equals(other: Any?): Boolean {
+		if (other !is VerseRef) return false
+		return (book == other.book &&
+			chapter == other.chapter &&
+			verse == other.verse)
+	}
+
+	override fun compareTo(other: VerseRef): Int {
+		return when {
+			book == other.book -> {
+				when {
+					chapter == other.chapter -> {
+						when {
+							verse == other.verse -> 0
+							verse < other.verse -> -1
+							else -> 1
+						}
+					}
+					chapter < other.chapter -> -1
+					else -> 1
+				}
+			}
+			book < other.book -> -1
+			else -> 1
 		}
 	}
 }
