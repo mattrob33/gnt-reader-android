e2b3e8bf53d04715ecaf908bf426ffcfe1c35ad9
Matt Robertson <mattrobertson333@gmail.com>
2020-10-14 14:07:03 -0500

    Initial commit of new classes

---
+++ app/src/main/java/com/mattrobertson/greek/reader/model/Verse.kt
@@ -0,0 +1,27 @@
+package com.mattrobertson.greek.reader.model
+
+/**
+ * A decoded representation of a single verse.
+ * @param verseRef validated verse reference (book, chapter, verse)
+ * @param words ordered list of Words for the verse
+ */
+data class Verse (
+	val verseRef: VerseRef,
+	val words: List<Word>
+) : Comparable<Verse> {
+	/**
+	 * Suitable for displaying to the user as the verse's text.
+	 */
+	val text: String by lazy {
+		val sb = StringBuilder()
+		words.forEach { word ->
+			sb.append(word.text).append(" ")
+		}
+		sb.toString().trimEnd()
+	}
+
+	override fun compareTo(other: Verse): Int {
+		return verseRef.compareTo(other.verseRef)
+	}
+
+}
