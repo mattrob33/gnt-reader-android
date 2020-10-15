371c858661c9a0f8c88f67ff31897b2ef1cdb2cc
Matt Robertson <mattrobertson333@gmail.com>
2020-10-14 22:20:28 -0500

    Fixed Room DB schema

--- app/src/main/java/com/mattrobertson/greek/reader/data/models/VerseEntity.kt
+++ app/src/main/java/com/mattrobertson/greek/reader/data/models/VerseEntity.kt
@@ -4,7 +4,10 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import com.mattrobertson.greek.reader.data.VerseDatabase.Companion.VERSES_TABLE
 
-@Entity(tableName = VERSES_TABLE)
+@Entity(
+	tableName = VERSES_TABLE,
+	primaryKeys = ["book", "chapter", "verse"]
+)
 data class VerseEntity (
 	val book: Int,
 	val chapter: Int,
