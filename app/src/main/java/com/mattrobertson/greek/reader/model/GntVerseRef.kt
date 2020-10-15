b3b8fd10b2ec6c3b232705bdfa16ef6e325697f7
Matt Robertson <mattrobertson333@gmail.com>
2020-10-15 11:39:56 -0500

    Replaced GntVerseRef with VerseRef

--- app/src/main/java/com/mattrobertson/greek/reader/model/GntVerseRef.kt
+++
@@ -1,9 +0,0 @@
-package com.mattrobertson.greek.reader.model
-
-import com.mattrobertson.greek.reader.util.AppConstants.NO_VERSE
-
-data class GntVerseRef (
-        val book: Int,
-        val chapter: Int,
-        val verse: Int = NO_VERSE
-)
