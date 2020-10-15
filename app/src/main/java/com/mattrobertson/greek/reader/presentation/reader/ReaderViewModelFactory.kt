8ffbb8207375a13fa5442f2658e8414df4d8560c
Matt Robertson <mattrobertson333@gmail.com>
2020-10-15 09:34:02 -0500

    ReaderViewModel now reads from Room DB and uses new Word model

--- app/src/main/java/com/mattrobertson/greek/reader/presentation/reader/ReaderViewModelFactory.kt
+++ app/src/main/java/com/mattrobertson/greek/reader/presentation/reader/ReaderViewModelFactory.kt
@@ -3,12 +3,13 @@ package com.mattrobertson.greek.reader.presentation.reader
 import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.mattrobertson.greek.reader.model.Book
 import com.mattrobertson.greek.reader.repo.VerseRepo
 
 class ReaderViewModelFactory(
         private val applicationContext: Context,
         private val verseRepo: VerseRepo,
-        private val book: Int,
+        private val book: Book,
         private val chapter: Int
 ): ViewModelProvider.Factory {
 
