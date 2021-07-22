ad687a9c020b34447442b649c8de896c13fcc929
Matt Robertson <mattrobertson333@gmail.com>
2021-07-22 10:46:45 -0500

    Fixed view binding in BottomNavHostFragment

--- app/src/main/java/com/mattrobertson/greek/reader/presentation/BottomNavHostFragment.kt
+++ app/src/main/java/com/mattrobertson/greek/reader/presentation/BottomNavHostFragment.kt
@@ -9,45 +9,45 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.mattrobertson.greek.reader.R
-import kotlinx.android.synthetic.main.bottom_nav_host_fragment.*
-
+import com.mattrobertson.greek.reader.databinding.BottomNavHostFragmentBinding
 
 class BottomNavHostFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.bottom_nav_host_fragment, container, false)
-    }
+    private var _binding: BottomNavHostFragmentBinding? = null
+    private val binding get() = _binding!!
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = BottomNavHostFragmentBinding.inflate(inflater, container, false)
 
-        (activity as AppCompatActivity).setSupportActionBar(toolbar)
+        (activity as AppCompatActivity).setSupportActionBar(binding.toolbar)
         (activity as AppCompatActivity).supportActionBar?.setDisplayShowTitleEnabled(false)
 
         val navController = findNavController(requireActivity(), R.id.nav_host_fragment)
-        bottom_nav_view.setupWithNavController(navController)
+        binding.bottomNavView.setupWithNavController(navController)
 
         navController.addOnDestinationChangedListener { _, destination, _ ->
 
             // Set toolbar title
             when (destination.id) {
-                R.id.navigation_plans -> toolbar_title.text = "Plans"
-                R.id.navigation_vocab -> toolbar_title.text = "Vocabulary"
-                R.id.navigation_more -> toolbar_title.text = "More"
+                R.id.navigation_plans -> binding.toolbarTitle.text = "Plans"
+                R.id.navigation_vocab -> binding.toolbarTitle.text = "Vocabulary"
+                R.id.navigation_more -> binding.toolbarTitle.text = "More"
             }
 
             // Swap toolbar title view (Reader uses a different view with a right drawable dropdown arrow)
             when (destination.id) {
                 R.id.navigation_read -> {
-                    toolbar_title.visibility = View.GONE
-                    toolbar_reader_title.visibility = View.VISIBLE
+                    binding.toolbarTitle.visibility = View.GONE
+                    binding.toolbarReaderTitle.visibility = View.VISIBLE
                 }
                 else -> {
-                    toolbar_reader_title.visibility = View.GONE
-                    toolbar_title.visibility = View.VISIBLE
+                    binding.toolbarReaderTitle.visibility = View.GONE
+                    binding.toolbarTitle.visibility = View.VISIBLE
                 }
             }
         }
+
+        return binding.root
     }
 
 }
