c049c2636bea73088a9729f6654f51a6e58ce748
Matt Robertson <mattrobertson333@gmail.com>
2021-07-27 14:05:13 -0500

    Added proto datastore Location recall

---
+++ app/src/main/java/com/mattrobertson/greek/reader/settings/ScrollLocation.kt
@@ -0,0 +1,34 @@
+package com.mattrobertson.greek.reader.settings
+
+import android.content.Context
+import androidx.datastore.core.CorruptionException
+import androidx.datastore.core.DataStore
+import androidx.datastore.core.Serializer
+import androidx.datastore.dataStore
+import com.google.protobuf.InvalidProtocolBufferException
+import com.mattrobertson.greek.reader.ScrollLocation
+import java.io.InputStream
+import java.io.OutputStream
+
+object ScrollLocationSerializer : Serializer<ScrollLocation> {
+
+    override val defaultValue: ScrollLocation = ScrollLocation.getDefaultInstance()
+
+    override suspend fun readFrom(input: InputStream): ScrollLocation {
+        try {
+            return ScrollLocation.parseFrom(input)
+        }
+        catch (e: InvalidProtocolBufferException) {
+            throw CorruptionException("Cannot read proto.", e)
+        }
+    }
+
+    override suspend fun writeTo(t: ScrollLocation, output: OutputStream) {
+        t.writeTo(output)
+    }
+}
+
+val Context.scrollLocationDataStore: DataStore<ScrollLocation> by dataStore(
+    fileName = "scroll_location.pb",
+    serializer = ScrollLocationSerializer
+)
