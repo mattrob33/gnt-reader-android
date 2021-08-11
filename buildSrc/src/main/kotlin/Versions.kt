7de821be1f7ff9fada3ba952acdbee9f5bb23527
Matt Robertson <mattrobertson333@gmail.com>
2021-08-11 09:39:58 -0500

    Started migrating to kts gradle

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,9 @@
+object Versions {
+    const val kotlin = "1.5.21"
+    const val agp = "7.0.0"
+
+    const val hilt = "2.37"
+    const val compose = "1.0.1"
+
+    const val hiltViewModels = "1.0.0-alpha03"
+}
