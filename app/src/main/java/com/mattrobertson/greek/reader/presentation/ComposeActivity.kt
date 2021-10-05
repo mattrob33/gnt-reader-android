920bf4b5044053ac8ecdb8f0e5aba891e0befc7e
Matt Robertson <mrobertson@crossway.org>
2021-10-05 12:18:48 -0500

    Fixed compilation by adding experimental api annotation

--- app/src/main/java/com/mattrobertson/greek/reader/presentation/ComposeActivity.kt
+++ app/src/main/java/com/mattrobertson/greek/reader/presentation/ComposeActivity.kt
@@ -3,6 +3,7 @@ package com.mattrobertson.greek.reader.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.material.ExperimentalMaterialApi
 import com.mattrobertson.greek.reader.ui.theme.AppTheme
@@ -11,6 +12,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class ComposeActivity : ComponentActivity() {
 
+    @ExperimentalAnimationApi
     @ExperimentalFoundationApi
     @ExperimentalMaterialApi
     override fun onCreate(savedInstanceState: Bundle?) {
