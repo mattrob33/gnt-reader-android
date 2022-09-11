739cb407825044316936cb1157a4573833e0cdeb
Matt Robertson <mattrobertson333@gmail.com>
2022-09-10 21:30:56 -0500

    Added underline to selected word

--- feature/reading/src/main/java/com/mattrobertson/greek/reader/reading/ui/ComposeReader.kt
+++ feature/reading/src/main/java/com/mattrobertson/greek/reader/reading/ui/ComposeReader.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.BaselineShift
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.zIndex
@@ -237,9 +238,21 @@ fun ChapterText(
                     else
                         FontWeight.Normal
 
-                withStyle(style = SpanStyle(fontWeight = wordFontWeight)) {
-                    append("${word.text} ")
+                val wordFontTextDecoration =
+                    if (isClicked)
+                        TextDecoration.Underline
+                    else
+                        TextDecoration.None
+
+                withStyle(
+                    style = SpanStyle(
+                        fontWeight = wordFontWeight,
+                        textDecoration = wordFontTextDecoration
+                    )
+                ) {
+                    append(word.text)
                 }
+                append(" ")
             }
 
             if (settings.versesOnNewLines) {
