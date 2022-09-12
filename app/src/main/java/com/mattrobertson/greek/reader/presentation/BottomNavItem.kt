fe709e46008e60076f629bb379a30e4df722de9f
Matt Robertson <mattrobertson333@gmail.com>
2022-09-12 12:48:51 -0500

    Tweaked bottom bar icons

--- app/src/main/java/com/mattrobertson/greek/reader/presentation/BottomNavItem.kt
+++ app/src/main/java/com/mattrobertson/greek/reader/presentation/BottomNavItem.kt
@@ -1,20 +1,17 @@
 package com.mattrobertson.greek.reader.presentation
 
-import androidx.annotation.StringRes
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.List
-import androidx.compose.material.icons.rounded.Settings
-import androidx.compose.material.icons.rounded.Speaker
+import androidx.compose.material.icons.rounded.*
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import com.mattrobertson.greek.reader.R
 import com.mattrobertson.greek.reader.SblGntApplication
 
-sealed class BottomNavItem(val route: String, @StringRes val label: Int, val icon: ImageVector) {
-    object Contents : BottomNavItem("contents", R.string.contents, Icons.Rounded.List)
-    object Vocab : BottomNavItem("vocab", R.string.vocab, ImageVector.vectorResource(null, SblGntApplication.context.resources, R.drawable.ic_vocab))
-    object Audio : BottomNavItem("audio", R.string.audio, Icons.Rounded.Speaker)
-    object Settings : BottomNavItem("settings", R.string.settings, Icons.Rounded.Settings)
+sealed class BottomNavItem(val route: String, val icon: ImageVector) {
+    object Contents : BottomNavItem("contents", Icons.Rounded.FormatListBulleted)
+    object Vocab : BottomNavItem("vocab", ImageVector.vectorResource(null, SblGntApplication.context.resources, R.drawable.ic_vocab))
+    object Audio : BottomNavItem("audio", Icons.Rounded.VolumeUp)
+    object Settings : BottomNavItem("settings", Icons.Rounded.Settings)
 }
 
 val bottomNavItems = listOf(
